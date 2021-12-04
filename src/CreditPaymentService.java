0e1f6ce57bb00d5548e070d302f41d7380f8c903
Andrey Gribanov <shade1471@gmail.com>
2021-12-04 21:08:44 +0700

    Reformat Code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,13 @@
 public class CreditPaymentService {
-    public double calculate(double rate,int time,long credit) {
+    public double calculate(double rate, int time, long credit) {
         // Расчет для аннуитетных платежей //
-
         // Месячная процентная ставка (= ставка по кредиту /100*12)
-        double mRate = rate / (100 * 12);
+        double monthlyRate = rate / (100 * 12);
 
         // Размер ежемесячного платежа
-        double monthlyPayment = credit*mRate/(1-Math.pow((1+mRate),-time));
+        double monthlyPayment = credit * monthlyRate / (1 - Math.pow((1 + monthlyRate), -time));
 
         return monthlyPayment;
+
     }
 }
